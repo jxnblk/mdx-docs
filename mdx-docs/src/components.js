7d1d67eec74e6423c28c2df0551775b672620c9a
Brent Jackson <jxnblk@gmail.com>
2018-08-18 14:05:56 -0400

    Add absolute url check to link

--- mdx-docs/src/components.js
+++ mdx-docs/src/components.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import NextLink from 'next/link'
 import get from 'lodash.get'
+import isAbsoluteURL from 'is-absolute-url'
 import LiveCode from './LiveCode'
 import css from './css'
 import { withDocs } from './context'
@@ -26,7 +27,7 @@ export const a = styled(withDocs(({
 }) =>
   <NextLink {...props}
     href={href}
-    as={isProd ? basepath + href : href}>
+    as={isProd && !isAbsoluteURL(href) ? basepath + href : href}>
     <a className={className}>
       {children}
     </a>
