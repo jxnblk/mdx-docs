45e4f98ddbe60ca395fcf2f4d4bd5063ba5e9b7d
Brent Jackson <jxnblk@gmail.com>
2018-08-30 11:16:12 -0400

    Adjust MenuButton and add active className to NavLink

--- mdx-docs/docs/App.js
+++ mdx-docs/docs/App.js
@@ -5,14 +5,35 @@ import {
   SideNav,
   Header,
   Pagination,
+  themes
 } from '../src'
 import Hello from './hello.mdx'
+import styled from 'styled-components'
+
+const theme = {
+  ...themes.default,
+  NavLink: {
+    '&.active': {
+      color: '#07c'
+    }
+  }
+}
 
 const routes = [
   { name: 'Home', path: '/' },
   { name: 'Getting Started', path: '/getting-started' },
   { name: 'API', path: '/api' },
 ]
+
+const Pad = styled.div({
+  paddingTop: '32px',
+  paddingBottom: '32px',
+})
+const sidebar = (
+  <Pad>
+    <SideNav />
+  </Pad>
+)
 
 export default () =>
   <Layout
@@ -21,9 +42,8 @@ export default () =>
       pathname: '/getting-started'
     }}
     routes={routes}
-    sidebar={(
-      <SideNav />
-    )}
+    theme={theme}
+    sidebar={sidebar}
     footer={(
       <Pagination />
     )}>
