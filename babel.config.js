851f16d3d3a199927a70b10096e812a4aaa1339d
Brent Jackson <jxnblk@gmail.com>
2018-10-05 13:52:30 -0400

    Setup tests in root

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    '@babel/env',
+    '@babel/react',
+  ],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-export-namespace-from',
+  ]
+}
