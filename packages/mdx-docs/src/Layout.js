243669d3c15d08dc895f498658918f5abb1aaaad
Brent Jackson <jxnblk@gmail.com>
2018-10-05 23:17:39 -0400

    Use mdx-style in mdx-docs

--- packages/mdx-docs/src/Layout.js
+++ packages/mdx-docs/src/Layout.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Root from './Root'
+import MDXStyle from 'mdx-style'
+import { base as baseTheme } from 'mdx-style/themes'
 import { DocsContext, withDocs } from './context'
+import defaultComponents from './components'
+
+console.log(defaultComponents)
 
 const MediaContext = React.createContext()
 
@@ -358,6 +362,8 @@ export class Layout extends React.Component {
       breakpoint,
       routes = [],
       router = {},
+      components = {},
+      theme = {},
       ...props
     } = this.props
 
@@ -386,11 +392,22 @@ export class Layout extends React.Component {
           }}>
           {menuToggle}
           {navbar}
-          <Root {...props}>
+          <MDXStyle
+            css={{
+              ...baseTheme,
+              // todo: fix
+              code: {},
+              // ...theme
+            }}
+            components={{
+              ...defaultComponents,
+              ...components
+            }}
+            {...props}>
             <Flex>
               {columns}
             </Flex>
-          </Root>
+          </MDXStyle>
         </MediaProvider>
       </DocsContext.Provider>
     )
